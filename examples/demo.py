d3901f70bd3a44ed6bddc7725db115802286b9c9
DSaurus <2238454358@qq.com>
2021-03-07 02:17:38 +0800

    optimization test

--- examples/demo.py
+++ examples/demo.py
@@ -22,6 +22,76 @@ import torch
 
 import smplx
 
+import taichi_three as t3
+import taichi as ti
+import cv2
+import torch.nn.functional as F
+
+
+class View:
+    def __init__(self, img, vid, joints_2d):
+        self.img = img
+        self.vid = vid
+        self.joints_2d = joints_2d
+        self.joints_3d = None
+        self.erase_id = -1
+        self.mask_id = np.ones(joints_2d.shape[0])
+        cv2.namedWindow('%d_view' % self.vid)
+        cv2.moveWindow('%d_view' % self.vid, self.vid % 3 * 512, self.vid // 3 * 512)
+    def init(self):
+        def call_back(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDBLCLK:
+                min_dis = 1e9
+                min_i = -1
+                for i in range(self.joints_2d.shape[0]):
+                    if (x - self.joints_2d[i, 0]) ** 2 + (y - self.joints_2d[i, 1]) ** 2 < min_dis:
+                        min_dis = (x - self.joints_2d[i, 0]) ** 2 + (y - self.joints_2d[i, 1]) ** 2
+                        min_i = i
+                self.mask_id[min_i] = 0
+                self.joints_2d[min_i, :] = 0
+                self.draw()
+            if event == cv2.EVENT_LBUTTONDOWN:
+                if self.erase_id != -1:
+                    self.joints_2d[self.erase_id, 0] = x
+                    self.joints_2d[self.erase_id, 1] = y
+                    self.erase_id = -1
+                    self.draw()
+            if event == cv2.EVENT_RBUTTONDOWN:
+                if self.erase_id == -1:
+                    min_dis = 1e9
+                    min_i = -1
+                    for i in range(self.joints_2d.shape[0]):
+                        if (x-self.joints_2d[i, 0])**2 + (y-self.joints_2d[i, 1])**2 < min_dis:
+                            min_dis = (x-self.joints_2d[i, 0])**2 + (y-self.joints_2d[i, 1])**2
+                            min_i = i
+                    self.erase_id = min_i
+                    self.draw()
+        cv2.setMouseCallback('%d_view' % self.vid, call_back)
+
+    def draw(self):
+        img = self.img.copy()
+        for i in range(self.joints_2d.shape[0]):
+            if i != self.erase_id:
+                cv2.circle(img, (int(self.joints_2d[i, 0]), int(self.joints_2d[i, 1])), 2, (0, 0, 255), 2)
+        edge = [[0, 1], [0, 2], [1, 4], [2, 5], [5, 8], [4, 7], [8, 11], [7, 10], [0, 3], [3, 6], [6, 9],
+                [9, 12], [12, 15], [9, 14],
+                [14, 17], [17, 19], [19, 21], [9, 13], [13, 16], [16, 18], [18, 20]]
+        if self.joints_3d is not None:
+            for i in range(self.joints_3d.shape[0]):
+                cv2.circle(img, (int(self.joints_3d[i, 0]), int(self.joints_3d[i, 1])), 2, (255, 0, 0), 2)
+            for e in edge:
+                cv2.line(img, (int(self.joints_3d[e[0], 0]), int(self.joints_3d[e[0], 1])),
+                         (int(self.joints_3d[e[1], 0]), int(self.joints_3d[e[1], 1])),
+                         (255, 0, 0), 2)
+
+        for e in edge:
+            if self.mask_id[e[0]] == 0 or self.mask_id[e[1]] == 0:
+                continue
+            cv2.line(img, (int(self.joints_2d[e[0], 0]), int(self.joints_2d[e[0], 1])), (int(self.joints_2d[e[1], 0]), int(self.joints_2d[e[1], 1])),
+                     (0, 255, 0), 2)
+        cv2.imshow('%d_view' % self.vid, img)
+
+
 
 def main(model_folder,
          model_type='smplx',
@@ -34,89 +104,209 @@ def main(model_folder,
          num_expression_coeffs=10,
          plotting_module='pyrender',
          use_face_contour=False):
-
     model = smplx.create(model_folder, model_type=model_type,
                          gender=gender, use_face_contour=use_face_contour,
                          num_betas=num_betas,
                          num_expression_coeffs=num_expression_coeffs,
                          ext=ext)
     print(model)
+    ti.init(ti.cpu)
+    obj = t3.readobj('dataset/650/smplx.obj')
+    obj['vi'][:, 0] = -obj['vi'][:, 0]
+    obj['vi'][:, 2] = -obj['vi'][:, 2]
+    scene = t3.Scene()
+    camera = t3.Camera()
+    scene.add_camera(camera)
+    light = t3.Light()
+    scene.add_light(light)
+    light2 = t3.Light([0, 0, -1])
+    scene.add_light(light2)
+    intrinsic = np.load('dataset/650/0_intrinsic.npy')
+    extrinsic = np.load('dataset/650/0_extrinsic.npy')
+    camera.set_intrinsic(intrinsic[0, 0], intrinsic[1, 1], intrinsic[0, 2], intrinsic[1, 2])
+    pos = -extrinsic[:3, :3].T @ extrinsic[:3, 3]
+    trans = extrinsic[:3, :3].T
+    camera.pos_py = [pos[i] for i in range(3)]
+    camera.trans_py = [[trans[i, j] for j in range(3)] for i in range(3)]
+
+    t3_m = t3.Model(obj=obj)
+    scene.add_model(t3_m)
+    camera._init()
+
+    gui = ti.GUI('smpl')
 
     betas, expression = None, None
     if sample_shape:
-        betas = torch.randn([1, model.num_betas], dtype=torch.float32)
+        betas = torch.randn([1, model.num_betas], dtype=torch.float32, requires_grad=True)
     if sample_expression:
         expression = torch.randn(
-            [1, model.num_expression_coeffs], dtype=torch.float32)
+            [1, model.num_expression_coeffs], dtype=torch.float32, requires_grad=True)
+    trans = torch.randn([1, 3], requires_grad=True)
+    global_o = torch.zeros(3).unsqueeze(0)
+    pose = torch.zeros((1, model.NUM_BODY_JOINTS*3))
+    global_o.require_grad = True
+    pose.requires_grad = True
+    print(global_o.shape, pose.shape)
 
-    output = model(betas=betas, expression=expression,
-                   return_verts=True)
+    optim = torch.optim.Adam([trans, global_o], lr=1e-1)
+    for i in range(300):
+        output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                       return_verts=True, global_orient=global_o)
+        verts = output.vertices
+        gt_v = torch.FloatTensor(obj['vi']).unsqueeze(0)
+        loss = torch.nn.functional.mse_loss(verts, gt_v)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        print(loss.item())
+        if i % 100 == 0:
+            vs = verts.detach().numpy().squeeze()
+            vs[:, 0] = -vs[:, 0]
+            vs[:, 2] = -vs[:, 2]
+            t3_m.vi.from_numpy(vs)
+            scene.render()  # render the model(s) into image
+            gui.set_image(camera.img)  # display the result image
+            gui.show()
+    optim = torch.optim.Adam([trans, global_o, pose], lr=1e-2)
+    for i in range(300):
+        output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                       return_verts=True, global_orient=global_o)
+        verts = output.vertices
+        gt_v = torch.FloatTensor(obj['vi']).unsqueeze(0)
+        loss = torch.nn.functional.mse_loss(verts, gt_v)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if i % 500 == 0:
+            print(i, loss.item())
+        if i % 30 == 0:
+            vs = verts.detach().numpy().squeeze()
+            vs[:, 0] = -vs[:, 0]
+            vs[:, 2] = -vs[:, 2]
+            t3_m.vi.from_numpy(vs)
+            scene.render()  # render the model(s) into image
+            gui.set_image(camera.img)  # display the result image
+            gui.show()
+    optim = torch.optim.Adam([trans, pose, global_o, betas, expression], lr=1e-2)
+    for i in range(1000):
+        output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                       return_verts=True, global_orient=global_o)
+        verts = output.vertices
+        gt_v = torch.FloatTensor(obj['vi']).unsqueeze(0)
+        loss = torch.nn.functional.mse_loss(verts, gt_v)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if i % 30 == 0:
+            vs = verts.detach().numpy().squeeze()
+            vs[:, 0] = -vs[:, 0]
+            vs[:, 2] = -vs[:, 2]
+            t3_m.vi.from_numpy(vs)
+            scene.render()  # render the model(s) into image
+            gui.set_image(camera.img)  # display the result image
+            gui.show()
+            print(loss.item())
+
+    output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                   return_verts=True, global_orient=global_o)
     vertices = output.vertices.detach().cpu().numpy().squeeze()
     joints = output.joints.detach().cpu().numpy().squeeze()
+    f = open('smplx.obj', 'w')
+    for i in range(vertices.shape[0]):
+        f.write('v %f %f %f\n' % (vertices[i, 0], vertices[i, 1], vertices[i, 2]))
 
-    print('Vertices shape =', vertices.shape)
-    print('Joints shape =', joints.shape)
+    for i in range(obj['f'].shape[0]):
+        f.write('f %d %d %d\n' % (obj['f'][i, 0, 0]+1, obj['f'][i, 1, 0]+1, obj['f'][i, 2, 0]+1))
 
-    if plotting_module == 'pyrender':
-        import pyrender
-        import trimesh
-        vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
-        tri_mesh = trimesh.Trimesh(vertices, model.faces,
-                                   vertex_colors=vertex_colors)
+    print(joints.shape)
+    joints[:, 0] *= -1
+    joints[:, 2] *= -1
+    view_list = []
+    ex_list = []
+    in_list = []
+    for vid in range(6):
+        intrinsic = np.load('dataset/650/%d_intrinsic.npy' % vid)
+        extrinsic = np.load('dataset/650/%d_extrinsic.npy' % vid)
+        pts = extrinsic[:3, :3] @ joints.T
+        pts += extrinsic[:3, 3:]
+        pts = intrinsic @ pts
+        pts[:2, :] /= pts[2:, :]
+        pts = pts.T
+        view = View(cv2.imread('dataset/650/%d.jpg' % vid), vid, pts[:22, :2])
+        view.init()
+        view.draw()
+        view_list.append(view)
+        ex_list.append(torch.from_numpy(extrinsic).float())
+        in_list.append(torch.from_numpy(intrinsic).float())
 
-        mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+    while 1:
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
 
-        scene = pyrender.Scene()
-        scene.add(mesh)
+    joints_3d = torch.from_numpy(joints[:22]).float()
+    joints_3d.requires_grad = True
+    optim = torch.optim.Adam([joints_3d], lr=1e-3)
+    for _ in range(1000):
+        loss = 0
+        for vid in range(6):
+            pts = ex_list[vid][:3, :3] @ joints_3d.T
+            pts += ex_list[vid][:3, 3:]
+            pts = in_list[vid] @ pts
+            pts2 = pts[:2, :] /pts[2:, :]
+            pts2 = pts2.T
+            mask = torch.FloatTensor(view_list[vid].mask_id).unsqueeze(1)
+            gt = torch.from_numpy(view_list[vid].joints_2d).float()
+            loss += F.mse_loss(pts2*mask / 512, gt*mask / 512)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if _ % 10 == 0:
+            for vid in range(6):
+                pts = ex_list[vid][:3, :3] @ joints_3d.T
+                pts += ex_list[vid][:3, 3:]
+                pts = in_list[vid] @ pts
+                pts[:2, :] /= pts[2:, :]
+                pts = pts.T
+                view_list[vid].joints_3d = pts.detach().cpu().numpy()
+                view_list[vid].draw()
+            if cv2.waitKey() & 0xFF == 27:
+                break
 
-        if plot_joints:
-            sm = trimesh.creation.uv_sphere(radius=0.005)
-            sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
-            tfs = np.tile(np.eye(4), (len(joints), 1, 1))
-            tfs[:, :3, 3] = joints
-            joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
-            scene.add(joints_pcl)
+    joints_3d.requires_grad = False
+    joints_3d[:, 0] = -joints_3d[:, 0]
+    joints_3d[:, 2] = -joints_3d[:, 2]
+    optim = torch.optim.Adam([trans, pose, global_o, betas, expression], lr=1e-2)
+    for i in range(1000):
+        output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                       return_verts=True, global_orient=global_o)
+        verts = output.vertices
+        joints = output.joints
+        loss = torch.nn.functional.mse_loss(joints[:, :22], joints_3d.unsqueeze(0))
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if i % 30 == 0:
+            vs = verts.detach().numpy().squeeze()
+            vs[:, 0] = -vs[:, 0]
+            vs[:, 2] = -vs[:, 2]
+            t3_m.vi.from_numpy(vs)
+            scene.render()  # render the model(s) into image
+            gui.set_image(camera.img)  # display the result image
+            gui.show()
+            print(loss.item())
 
-        pyrender.Viewer(scene, use_raymond_lighting=True)
-    elif plotting_module == 'matplotlib':
-        from matplotlib import pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+    output = model(betas=betas, expression=expression, body_pose=pose, transl=trans,
+                   return_verts=True, global_orient=global_o)
+    vertices = output.vertices.detach().cpu().numpy().squeeze()
+    f = open('smplx.obj', 'w')
+    for i in range(vertices.shape[0]):
+        f.write('v %f %f %f\n' % (-vertices[i, 0], vertices[i, 1], -vertices[i, 2]))
+    for i in range(obj['f'].shape[0]):
+        f.write('f %d %d %d\n' % (obj['f'][i, 0, 0] + 1, obj['f'][i, 1, 0] + 1, obj['f'][i, 2, 0] + 1))
+    #
+    # print('Vertices shape =', vertices.shape)
+    # print('Joints shape =', joints.shape)
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        mesh = Poly3DCollection(vertices[model.faces], alpha=0.1)
-        face_color = (1.0, 1.0, 0.9)
-        edge_color = (0, 0, 0)
-        mesh.set_edgecolor(edge_color)
-        mesh.set_facecolor(face_color)
-        ax.add_collection3d(mesh)
-        ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], color='r')
-
-        if plot_joints:
-            ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], alpha=0.1)
-        plt.show()
-    elif plotting_module == 'open3d':
-        import open3d as o3d
-
-        mesh = o3d.geometry.TriangleMesh()
-        mesh.vertices = o3d.utility.Vector3dVector(
-            vertices)
-        mesh.triangles = o3d.utility.Vector3iVector(model.faces)
-        mesh.compute_vertex_normals()
-        mesh.paint_uniform_color([0.3, 0.3, 0.3])
-
-        geometry = [mesh]
-        if plot_joints:
-            joints_pcl = o3d.geometry.PointCloud()
-            joints_pcl.points = o3d.utility.Vector3dVector(joints)
-            joints_pcl.paint_uniform_color([0.7, 0.3, 0.3])
-            geometry.append(joints_pcl)
-
-        o3d.visualization.draw_geometries(geometry)
-    else:
-        raise ValueError('Unknown plotting_module: {}'.format(plotting_module))
 
 
 if __name__ == '__main__':
@@ -127,7 +317,7 @@ if __name__ == '__main__':
     parser.add_argument('--model-type', default='smplx', type=str,
                         choices=['smpl', 'smplh', 'smplx', 'mano', 'flame'],
                         help='The type of model to load')
-    parser.add_argument('--gender', type=str, default='neutral',
+    parser.add_argument('--gender', type=str, default='male',
                         help='The gender of the model')
     parser.add_argument('--num-betas', default=10, type=int,
                         dest='num_betas',
